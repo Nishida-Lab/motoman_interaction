d2a648e22e7cacc25113f51ddfea03f9e475ad4d
NishidaTakeshiLab <nishidatakeshilab@gmail.com>
2018-01-18 18:23:57 +0900

    [modify] subscribe ImageArray and publish StringArray

--- color_recognition/scripts/color_subscriber.py
+++ color_recognition/scripts/color_subscriber.py
@@ -15,30 +15,30 @@ class ColorDisplayNode:
 
     def __init__(self):
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("web_cam/image_raw", Image, self.image_callback, queue_size=1)
+        # self.image_sub = rospy.Subscriber("web_cam/image_raw", Image, self.image_callback, queue_size=1)
         self.person_sub = rospy.Subscriber('color', StringArray, self.color_callback, queue_size=1)
         self.is_update_image = False
         self.output=[]
 
-    def image_callback(self,data):
-        self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        self.is_update_image = True
+    # def image_callback(self,data):
+    #     self.cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    #     self.is_update_image = True
 
     def color_callback(self,data):
         # self.output = data.data
         self.output = copy.deepcopy(data.strings)
         # self.output = "aaaaaaa"
-        if not self.is_update_image:
-            return
+        # if not self.is_update_image:
+        #     return
         self.once()
 
     def once(self):
-        frame = self.cv_image
-        font = cv2.FONT_HERSHEY_PLAIN
-        cv2.putText(frame,self.output[0],(10,400),font, 5,(255,255,0))
-        cv2.imshow('output_frame',frame)
-        cv2.waitKey(3)
-        rospy.loginfo(self.output[0]+" is subscribed.")
+        # frame = self.cv_image
+        # font = cv2.FONT_HERSHEY_PLAIN
+        # cv2.putText(frame,self.output[0],(10,400),font, 5,(255,255,0))
+        # cv2.imshow('output_frame',frame)
+        # cv2.waitKey(3)
+        rospy.loginfo(self.output)
 
 def main():
     person_display_node = ColorDisplayNode()
