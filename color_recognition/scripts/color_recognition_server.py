8235a1fe563d3c1bc23e36470fdb29b4f0e7c610
RyodoTanaka <k104073r@mail.kyutech.jp>
2018-02-15 23:15:10 +0900

    [modify] error correction

--- color_recognition/scripts/color_recognition_server.py
+++ color_recognition/scripts/color_recognition_server.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import rospy
 
-from motoman_interaction_msgs.msg import StringArray
+from motoman_interaction_msgs.msg import StringArrayWithStatus
 from motoman_interaction_msgs.msg import ImageArray
 from motoman_interaction_msgs.srv import *
 
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
-from recognition_class import *
+from recognition_class_ import *
 
 
 class ColorRecognitionServer:
@@ -16,10 +16,10 @@ class ColorRecognitionServer:
     def __init__(self):
 
         rospy.init_node('color_recongnition_server')
-        s = rospy.Service('color_recongnition', ImageRecognition, self.callback)
+        s = rospy.Service('color_recongnition', ImageRecognitionWithStatus, self.callback)
 
         self.bridge = CvBridge()
-        self.detection = ColorRecognition()
+        self.recognition = ColorRecognition()
         self.image_array = []
         print "waiting for client to connect"
 
@@ -27,35 +27,39 @@ class ColorRecognitionServer:
     def get_color_string_array(self,ros_image_array):
 
         color_string_array = []
+        status_array = []
         image_cnt = 0
 
         for ros_image in ros_image_array:
             try:
                 cv_image = self.bridge.imgmsg_to_cv2(ros_image, "8UC3")
-                color_string_array.append(self.detection(cv_image))
+                recognition_result, recognition_status = self.recognition(cv_image)
+                color_string_array.append(recognition_result)
+                status_array.append(recognition_status)
 
                 cv2.imwrite(str(image_cnt)+".jpg", cv_image)
                 image_cnt += 1
 
             except CvBridgeError as e:
                 print(e)
-                return False
+                color_string_array.append("error")
+                status_array.append(0)
 
-        return color_string_array
+        return color_string_array, status_array
 
 
     def callback(self,data):
-        color_msg = StringArray()
+        color_msg = StringArrayWithStatus()
         self.image_array = copy.deepcopy(data.images.images)
 
         color_msg.header.stamp = data.images.header.stamp
         color_msg.header.frame_id = data.images.header.frame_id
-        color_msg.strings = self.get_color_string_array(self.image_array)
+        color_msg.strings, color_msg.status = self.get_color_string_array(self.image_array)
 
         rospy.loginfo(color_msg.header.frame_id)
         rospy.loginfo(color_msg.strings)
 
-        return ImageRecognitionResponse(color_msg)
+        return ImageRecognitionWithStatusResponse(color_msg)
 
 
 def main():
