8ce305748680ccfd4b662d67c945494e7d9e7f87
NishidaTakeshiLab <nishidatakeshilab@gmail.com>
2018-07-23 22:22:34 +0900

    [modify] rename the file name

--- ar_marker_tracking/scripts/tf_subscriber.py
+++ ar_marker_tracking/scripts/tf_subscriber.py
@@ -12,7 +12,7 @@ import numpy as np
 
 from motoman_interaction_msgs.msg import Teaching3D
 
-import csv
+
 # import std_msgs 
 
 class SendState:
@@ -22,113 +22,71 @@ class SendState:
         self.state_sub = rospy.Subscriber("visualization_marker", Marker, self.callback)
         self.state_pub = rospy.Publisher('/status', Teaching3D, queue_size=1)
 
+
         self.teaching_command = Teaching3D()
 
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listner = tf2_ros.TransformListener(self.tf_buffer)
 
-        # self.Initialize(self)
-        
-        self.trans = list()
-        
-        # self.position_x = trans.transform.translation.x
-        # self.position_y = trans.transform.translation.y
-        # self.position_z = trans.transform.translation.z
 
-    def getTF(self, marker):
+    def callback(self, message):
 
-        get_tf_flg = False
-
-        while not get_tf_flg:
-            try:
-                transformation = self.tf_buffer.lookup_transform('world', 'ar_marker_0',rospy.Time(0))
-                position_x = transformation.transform.translation.x
-                get_tf_flg = True
-                
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                continue
-
-        return transformation
-
-    def Initialize(self, maker):
-
-        init_position = False
-        
-        while not init_position:
-            for i in range(20):
-
-                self.trans.append(self.getTF(i+1, marker.header.stamp))
-                
-                if np.all(np.diff(trans) < 0.01):
-                    init_x = self.transformation.transform.translation.x
-                    init_position = True
-                else:
-                    continue
-
-        return init_x
-    
-    
-    def callback(self, marker):
-       
         rate = rospy.Rate(10.0)
-
-        print "Searching initial position..."
-        Initialize(self.trsnsformation)
-        rospy.sleep(2)
-        print "Set the initial psition."
-        rospy.sleep(2)
-        
-        get_trajectory = False
-        movement = False
-
-        while not get_trajectory:
+        while not rospy.is_shutdown():
             try:
-                print "Stay..."
+                # (trans,rot) = listener.lookupTransform('world', 'ar_marker_0',rospy.Time(0))
+                trans = self.tf_buffer.lookup_transform('world', 'ar_marker_0',rospy.Time(0))
+                # print
+                # print "raw_trans_data"
+                # print trans
+                # print
+                # print "translation"
+                # print trans.transform.translation
+                # print
+                # print "rotation"
+                # print trans.transform.rotation
+                # print
 
-                if abs(position_x - 0.325) >= 0.02 :
-                    print "Moving!"
-                # if ( abs(position_x - 0.325) >= 0.02 & abs(position_y - init_y)  & abs(position_z - init_z) ) :
-                    while not movement:
-
-                        # getTF(self.maker)
-                        self.trans.append(self.getTF(self.transformation.transform.translation.x))
-                        # trans = self.tf_buffer.lookup_transform('world', 'ar_marker_0',rospy.Time(0))
-                        # position_x = trans.transform.translation.x
-
-                        if abs(self.transformation.transform.translation.x - 0.100) <= 0.007:
-                            print "Finish!"
-                            movement = True
-                            get_trajectory= True
-                            break
-
-                # else:
-                #     print "Stay..."
-                
                 # trans_lists = []
                 
                 # for i in range(100000000):
                 #     trans_lists.append(trans.transform.translation.x)
+
+                    # print "Appending translations"
+                    # print trans_lists
+
+                    # diff_x = np.diff(trans_lists)
+                    # print ("diff= "+str(diff_x))
+
+                    # if diff_x.any(deif_x < 0.05) = True:
+                    #     print "Moving..."
+                    # else:
+                    #     continue
+
+                    # print ((diff_x > 0.05).any())
+                    # print (np.any(diff_x > 0.05))                  
+
+                position_x = trans.transform.translation.x
+
+                print position_x
                 
-                # print "Appending translations"
-                # print trans_lists
-                
-                # diff_x = np.diff(trans_lists)
-                # print ("diff= "+str(diff_x))
-                
-                # if diff_x.any(deif_x < 0.05) = True:
-                #     print "Moving..."
-                # else:
-                #     continue
-                
-                # print ((diff_x > 0.05).any())
-                # print (np.any(diff_x > 0.05))                  
-                
+                if abs(position_x - 0.325) >= 0.02:
+                    print "Moving!"
+                    trajectory_lists = []
+                    for i in range(10000):
+                        trajectory_lists.append(position_x)
+                        if abs(position_x - 0.100) <= 0.007:
+                            print "Finish!"
+                            break
+                else:
+                    print "Stay..."
+
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
 
-        print "Finish the task"
-
-        # rospy.is_shutdown()
+            else:
+                print "Finish the task"
+                break
             
             # try:
             #     trans_lists = []
@@ -143,6 +101,12 @@ class SendState:
             # np.diff(list, n=1, axis=-1)
 
             
+        # init_bottun = raw_input('>>> ')
+
+        # init_bottun == '0'
+
+        # print state_array_x
+        
 if __name__ == '__main__':
     rospy.init_node('tf_subscriber', anonymous=True)
     print("init done")
